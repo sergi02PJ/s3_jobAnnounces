5e6aa02eb3f95657560acd5749e610708873babf
Jaareet <80985233+Jaareet@users.noreply.github.com>
2022-08-12 17:39:02 +0100

    ESX removed, and AddEventHandler it is useless

--- client/client.lua
+++ client/client.lua
@@ -1,13 +1,3 @@
-ESX  = nil
-
-Citizen.CreateThread(function ()
-	  while ESX == nil do
-		  TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		  Citizen.Wait(0)
-  	end
-end)
-
-RegisterNetEvent('s3_jobAnuncios:SendAlert')
-AddEventHandler('s3_jobAnuncios:SendAlert', function(icon, texto)
-	  SendNUIMessage({ anuncio = true, admin = false, job = icon, texto = texto })
+RegisterNetEvent('s3_jobAnuncios:SendAlert', function(icon, texto)
+  SendNUIMessage({ anuncio = true, admin = false, job = icon, texto = texto })
 end)
