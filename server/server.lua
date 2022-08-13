b5fb15d533c368efb957abbd84be76a5ec58a37d
Jaareet <80985233+Jaareet@users.noreply.github.com>
2022-08-13 01:12:41 +0100

    Updated all serverside, new functions and etc...

--- server/server.lua
+++ server/server.lua
@@ -1,35 +1,55 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports.es_extended:getSharedObject()
 
-RegisterCommand('anuncioOn', function(source, args)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	
-	sendAnnounce(xPlayer, "textOn")
-end, false)
+players = {  }
 
-RegisterCommand('anuncioOff', function(source, args)
-    local xPlayer = ESX.GetPlayerFromId(source)
-	
-    sendAnnounce(xPlayer, "textOff")
-end, false)
+RegisterNetEvent('esx:playerLoaded', function(src, xPlayer)
+    players[src] = xPlayer
+end)
 
-sendAnnounce = function(xPlayer, type)
-    local playerJob = xPlayer.getJob()
+RegisterNetEvent('esx:playerDropped', function(src)
+    players[src] = nil
+end)
 
-	  if playerJob then
-		    local jobName = playerJob.name
-		    if Config.Announces[jobName] then
-            local announce = Config.Announces[jobName]
-            local adType = announce[type]
+RegisterNetEvent('onResourceStart', function(resourceName)
+    if resourceName == GetCurrentResourceName() then
+        for k, v in pairs(ESX.GetPlayers()) do
+            players[v] = ESX.GetPlayerFromId(v)
+        end
+    end
+end)
 
-            if xPlayer.getAccount(announce.account).money >= announce.cost then
-                xPlayer.removeAccountMoney(announce.account, announce.cost)
-                TriggerClientEvent('s3_jobAnuncios:SendAlert', -1, announce.icon, adType)
-            else
-                xPlayer.showNotification(Config.Translations.noMoney)
+RegisterCommand('add', function(source, args)
+    local announceType = ''
+
+    if args[1] then
+        if args[1] == 'open' then
+            announceType = 'textOn'
+        elseif args[1] == 'close' then
+            announceType = 'textOff'
+        end
+
+        sendAnnounce(source, announceType)
+    else
+        players[source].showNotification('Argumentos invalidos, debes de introducir /add open o /add close')
+    end
+end)
+
+function sendAnnounce(source, type)
+    local playerJob = players[source].getJob()
+    local jobName = playerJob.name
+    if Config.Announces[jobName] then
+        local announce = Config.Announces[jobName]
+        local adType = announce[type]
+
+        if players[source].getAccounts(announce.account).money >= announce.cost then
+            players[source].removeAccountMoney(announce.account, announce.cost)
+            for k, v in pairs(players) do
+                v.triggerEvent('s3_jobAnuncios:SendAlert', announce.icon, adType) 
             end
-		    else
-			      xPlayer.showNotification(Config.Translations.noJob)
-		    end
-	  end
+        else
+            players[source].showNotification(Config.Translations.noMoney) 
+        end
+    else
+        players[source].showNotification(Config.Translations.noJob)
+    end
 end
