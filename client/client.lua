3a1af1e9ae299467b0eff0c0e7aea55f439ac6b1
visibait <grafoplays@gmail.com>
2022-09-08 20:08:49 +0200

    tweak(client): Don't send admin variable as it's not used for anything at all

--- client/client.lua
+++ client/client.lua
@@ -1,4 +1,4 @@
 RegisterNetEvent('s3_jobAnuncios:SendAlert')
 AddEventHandler('s3_jobAnuncios:SendAlert', function(icon, texto)
-	SendNUIMessage({ anuncio = true, admin = false, job = icon, texto = texto })
+	SendNUIMessage({ anuncio = true, job = icon, texto = texto })
 end)
